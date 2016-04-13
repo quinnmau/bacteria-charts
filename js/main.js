ed5e9f694d7324957640992d45d67dca5cf70a35
quinnmau <mauq@uw.edu>
2016-04-13 11:39:48 -0700

    need to add page styles and legend

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ $(function() {
         buildGraph1(data, 'plot1');
         getDataBox2('Penicilin', data);
         buildBarGraph3(data, 'plot2');
+        buildbargraph2(data, 'plot3', 'Penicilin');
+        buildbargraph2(data, 'plot4', 'Neomycin');
+        buildbargraph2(data, 'plot5', "Streptomycin ");
     });
     
     //puts data into an object
@@ -25,6 +28,89 @@ $(function() {
         });
         return dataBox;
     }
+    
+    function getDataBox2(drugName, data) {
+        var dataBox = {};
+        var data1 = data;
+        data1.sort(function(a, b) {
+            if (a["Gram Staining "] === b["Gram Staining "]) {
+                return a["Bacteria "].localeCompare(b["Bacteria "]);
+            }
+            return a["Gram Staining "].localeCompare(b["Gram Staining "]);
+        });
+        data.forEach(function(d) {
+            dataBox[d[drugName]] = {
+                bacteria: d["Bacteria "],
+                stain: d["Gram Staining "]
+            }
+        });
+        return dataBox;
+    }
+    
+    //formats object from getDataBox1 to be passed to buildbargraph2
+    function formatTrace2(drugName, data) {
+        var dataBox = getDataBox1(drugName, data);
+        var keys = Object.keys(dataBox);
+        //sorts keys first by stain then by alphabetical order
+        keys.sort(function(a, b) {
+            var obj1 = dataBox[a];
+            var obj2 = dataBox[b];
+            return obj1["mic"] - obj2["mic"];
+        });
+        console.log(keys);
+        var values = keys.map(function(d) {
+            var obj = dataBox[d];
+            return obj.mic;
+        });
+        var trace = {
+            x: keys,
+            y: values,
+            type: 'bar',
+            marker: {
+                color: keys.map(function(d) {
+                    var obj = dataBox[d];
+                    if (obj.stain == "positive") {
+                        return '#501DA6';
+                    } else {
+                        return '#F51BDC';
+                    }
+                }),
+                line: {
+                    color: keys.map(function(d) {
+                        var obj = dataBox[d];
+                        if (obj.stain == "positive") {
+                            return '#501DA6';
+                        } else {
+                            return '#F51BDC';
+                        }
+                    }),
+                    width: 1.5
+                }
+            }
+        };
+        return trace;
+    }
+    
+    function buildbargraph2(data, plot, drugName) {
+        var traces = [formatTrace2(drugName, data)];
+        var layout = {
+            title: drugName,
+            xaxis: {
+                title: 'Bacteria',
+                showticklabels: false
+            },
+            yaxis: {
+                title: 'MIC (Drug Effectiveness)',
+                range: [0, 25]
+            },
+            margin: {
+                b: 120
+            }
+        }
+        Plotly.newPlot(plot, traces, layout, {staticPlot: true});
+    }
+    
+    /*---------------------------------------------------------------------*/ 
     
     //formats object from getDataBox so plotly can graph it in buildgraph1
     function formatTrace1(drugName, data) {
@@ -58,60 +144,23 @@ $(function() {
         return trace;
     }
     
-    function getDataBox2(drugName, data) {
-        var dataBox = {};
-        var data1 = data;
-        data1.sort(function(a, b) {
-            if (a["Gram Staining "] === b["Gram Staining "]) {
-                return a["Bacteria "].localeCompare(b["Bacteria "]);
-            }
-            return a["Gram Staining "].localeCompare(b["Gram Staining "]);
-        });
-        console.log(data1);
-        data.forEach(function(d) {
-            dataBox[d[drugName]] = {
-                bacteria: d["Bacteria "],
-                stain: d["Gram Staining "]
-            }
-        });
-        return dataBox;
-    }
-    
-    //formats object from getDataBox1 to be passed to buildbargraph2
-    function formatTrace2(drugName, data) {
-        var dataBox = getDataBox1(drugName, data);
-        var keys = Object.keys(dataBox);
-        //sorts keys first by stain then by alphabetical order
-        keys.sort(function(a, b) {
-            var obj1 = dataBox[a];
-            var obj2 = dataBox[b];
-            if (obj1["stain"] === obj2["stain"]) {
-                return a.localeCompare(b);
-            } else {
-                return obj1["stain"].localeCompare(obj2["stain"]);
-            }
-        });
-        console.log(keys);
-        var values = keys.map(function(d) {
-            var obj = dataBox[d];
-            return obj.mic;
-        });
-        var trace = {
-            x: keys,
-            y: values,
-            type: 'bar',
-            marker: {
-                color: keys.map(function(d) {
-                    var obj = dataBox[d];
-                    if (obj.stain == "positive") {
-                        return '#501DA6';
-                    } else {
-                        return '#F51BDC';
-                    }
-                })
+    //builds the first graph
+    function buildGraph1(data, plot) {
+        var traces = [formatTrace1('Penicilin', data), formatTrace1("Streptomycin ", data), formatTrace1('Neomycin', data)];
+        var layout = {
+            title: 'Most Effective Drug for Each Bacteria',
+            margin: {
+                b: 120
+            },
+            xaxis: {
+                title: 'Bacteria'
+            },
+            yaxis: {
+                title: 'MIC (Drug Effectiveness)',
+                range: [-1, 12]
             }
         };
-        return trace;
+        Plotly.newPlot(plot, traces, layout, {staticPlot: true});
     }
     
     function formatTrace3(drugName, data) {
@@ -146,25 +195,6 @@ $(function() {
             name: drugName
         };
         return trace;
-    }
-    
-    //builds the first graph
-    function buildGraph1(data, plot) {
-        var traces = [formatTrace1('Penicilin', data), formatTrace1("Streptomycin ", data), formatTrace1('Neomycin', data)];
-        var layout = {
-            title: 'Most Effective Drug for Each Bacteria',
-            margin: {
-                b: 120
-            },
-            xaxis: {
-                title: 'Bacteria'
-            },
-            yaxis: {
-                title: 'MIC (Drug Effectiveness)',
-                range: [-1, 12]
-            }
-        };
-        Plotly.newPlot(plot, traces, layout);
     }
     
     function buildBarGraph3(data, plot) {
